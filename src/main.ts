c1a3d7885c4f290e93aacbd2abce7a499833c25f
MonayemSarker <bsse1228@iit.du.ac.bd>
2025-04-20 04:24:49 +0600

    fix: swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // const swaggerConfig = new DocumentBuilder()
-  //   .setTitle('E-Commerce API')
-  //   .setDescription('Manush Tech E-Commerce API Documentation')
-  //   .setVersion('1.0')
-  //   .addBearerAuth(
-  //     {
-  //       type: 'http',
-  //       scheme: 'bearer',
-  //       bearerFormat: 'JWT',
-  //       name: 'JWT',
-  //       description: 'Enter JWT Token',
-  //       in: 'header',
-  //     },
-  //     'JWT-auth',
-  //   )
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, swaggerConfig);
-  // SwaggerModule.setup('api-docs', app, document);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('E-Commerce API')
+    .setDescription('Manush Tech E-Commerce API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT Token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT);
   console.log('app running on port: ', process.env.PORT);
--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,46 +1,46 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { AuthGuard } from '@nestjs/passport';
-// import {
-//   ApiTags,
-//   ApiBearerAuth,
-//   ApiOperation,
-//   ApiResponse,
-// } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
-// @ApiTags('Reports')
+@ApiTags('Reports')
 @UseGuards(AuthGuard('jwt'))
-// @ApiBearerAuth('JWT-auth')
+@ApiBearerAuth('JWT-auth')
 @Controller('report')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
   @Get('monthly-sales')
-  // @ApiOperation({ summary: 'Get monthly sales report' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Monthly sales report',
-  // })
+  @ApiOperation({ summary: 'Get monthly sales report' })
+  @ApiResponse({
+    status: 200,
+    description: 'Monthly sales report',
+  })
   async getMonthlySalesReport() {
     return this.reportService.getMonthlySalesReport();
   }
 
   @Get('user-orders')
-  // @ApiOperation({ summary: 'Get user-wise order report' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'User-wise order report',
-  // })
+  @ApiOperation({ summary: 'Get user-wise order report' })
+  @ApiResponse({
+    status: 200,
+    description: 'User-wise order report',
+  })
   async getUserOrderReport() {
     return this.reportService.getUserOrderReport();
   }
 
   @Get('product-sales')
-  // @ApiOperation({ summary: 'Get product-wise sales report' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Product-wise sales report',
-  // })
+  @ApiOperation({ summary: 'Get product-wise sales report' })
+  @ApiResponse({
+    status: 200,
+    description: 'Product-wise sales report',
+  })
   async getProductSalesReport() {
     return this.reportService.getProductSalesReport();
   }
